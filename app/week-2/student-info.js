455c6f558ed3b202e127ae1037fba18870fcab9b
Jonathan Deleff <130598106+JonathanDeleff@users.noreply.github.com>
2024-01-16 14:29:27 -0700

    added the work for week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export const StudentInfo = () => {
+    return(
+      <Link href="https://github.com/JonathanDeleff/cprg306-assignments">Jonathan Deleff</Link>
+    )
+  }

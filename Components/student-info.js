bb81276346bc84d3483920aa3bff68d2931fbb52
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-01-18 13:07:13 -0700

    minor reformatting

--- Components/student-info.js
+++ Components/student-info.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 
 export const StudentInfo = () => {
-    return(
-      <Link href="https://github.com/JonathanDeleff/cprg306-assignments">Jonathan Deleff</Link>
-    )
-  }
+  return (
+    <Link href="https://github.com/JonathanDeleff/cprg306-assignments">
+      Jonathan Deleff
+    </Link>
+  );
+};

bf81641133e7b3e41c5d12f0aa108fb1a6e90dce
Jonathan Deleff <jonathan.deleff@edu.sait.ca>
2024-02-04 19:01:26 -0700

    one tiny fix in h1

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ import { Item } from "@/Components/item";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1>Shopping List:</h1>
+      <h1 className="text-3xl">Shopping List:</h1>
       <div>
         <Item />
       </div>
